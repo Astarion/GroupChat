b2200205057515aed18364ca70d4fadb4c827e29
Astarion <alexander.malgavka@gmail.com>
2017-03-02 23:11:03 +0300

    Parameters from arguments in server and client

--- src/Server.java
+++ src/Server.java
@@ -6,15 +6,26 @@ import java.net.Socket;
  * Created by 14Malgavka on 10.02.2017.
  */
 public class Server {
+    private static int sessionId = 0;
+    private static int sessionCounter = 0;
+    private static int maxSessionCount = 5;
     public static void main(String[] args) throws IOException {
         try {
 
             Integer port = Integer.parseInt(args[0]);
             ServerSocket serverSocket = new ServerSocket(port);
             while (true) {
-                Socket socket = serverSocket.accept();
-                Thread thread = new Thread(new Session(socket));
-                thread.start();
+
+                if (sessionCounter < maxSessionCount) {
+                    sessionId++;
+                    sessionCounter++;
+                    Socket socket = serverSocket.accept();
+                    Thread thread = new Thread(new Session(socket));
+                    System.out.println("Number of session: " + sessionCounter);
+                    thread.start();
+                }
+
+
             }
 //            InputStream inputStream = socket.getInputStream();
 //            DataInputStream dataInputStream = new DataInputStream(inputStream);
@@ -32,7 +43,14 @@ public class Server {
         }
 
 
+
     }
+    public void threadStop(){
+        sessionCounter--;
+        System.out.println("Number of session: " + sessionCounter);
+    }
+
+
 }
 // Сделать счетчик, который контролирует количество одновременных подключений
 // Если пытаается подключиться, когда нет места, ему выводится сообщение "Подожди"
